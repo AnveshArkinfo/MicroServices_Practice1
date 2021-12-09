5e1d44ea427a750e862b72810801043cf93456c7
AnveshArkinfo <anveshkumarinfo@gmail.com>
2021-12-09 21:34:34 +0530

    Second commit

--- Movie-Catalog-Service/src/main/java/com/MicroServices/MovieCatalogService/Resource/MovieCatalogResource.java
+++ Movie-Catalog-Service/src/main/java/com/MicroServices/MovieCatalogService/Resource/MovieCatalogResource.java
@@ -1,24 +1,37 @@
 package com.MicroServices.MovieCatalogService.Resource;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.MicroServices.MovieCatalogService.Model.CatalogItem;
+import com.MicroServices.MovieCatalogService.Model.Movie;
+import com.MicroServices.MovieCatalogService.Model.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
 		
-		return Collections.singletonList(
-				new CatalogItem("Age of Ultron", "Robotic Sci-Fi superhero film", 4)
-				);
+		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" +userId, UserRating.class);
+		
+		return ratings.getUserRating().stream().map(rating -> {
+			
+		Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getRating(), Movie.class);
+		return new CatalogItem(movie.getName() , "Sample Desc", rating.getRating());
+		})
+			.collect(Collectors.toList());
+
 	}
 	
 }
--- Rating-Data-Service/src/main/java/com/MicroServices/RatingDataService/Resource/RatingResource.java
+++ Rating-Data-Service/src/main/java/com/MicroServices/RatingDataService/Resource/RatingResource.java
@@ -1,19 +1,33 @@
 package com.MicroServices.RatingDataService.Resource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.MicroServices.RatingDataService.Model.Rating;
+import com.MicroServices.RatingDataService.Model.UserRating;
 
 @RestController
-@RequestMapping("/ratings")
+@RequestMapping("/ratingsdata")
 public class RatingResource {
 	
-	@RequestMapping("/{movieName}")
-	public Rating getRatings(@PathVariable String movieName) {
-		
-		return new Rating(movieName, 4);
+	@RequestMapping("/{movieId}")
+	public Rating getRatings(@PathVariable String movieId) {
+		return new Rating(movieId, 4);
+	}
+	
+	@RequestMapping("/users/{userId}")
+	public UserRating getUserRating(@PathVariable String userId) {
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 3)
+				);
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
 	}
 	
 }
